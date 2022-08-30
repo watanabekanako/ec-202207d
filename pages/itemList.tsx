781efc06913c742d7ebf36b45dc7284bdbd87a0d
emiri yasuo <emiri.yasuo@rakus-partners.co.jp>
2022-08-30 16:39:55 +0900

    １−３ほとんど完成（価格の安い順ができていない）

--- pages/itemList.tsx
+++ pages/itemList.tsx
@@ -1,67 +1,100 @@
 import React from 'react';
 import Head from 'next/head';
-import Link from 'next/link';
-import ItemList from '../components/ItemList';
+// import Link from 'next/link';
+// import ItemList from '../components/ItemList';
 import { useState, useEffect } from 'react';
-import useSWR, { useSWRConfig } from 'swr'
+import useSWR, { useSWRConfig } from 'swr';
+import Image from 'next/image';
 
-const fetcher = (url:any)=> fetch(url).then((res) => res.json());
+const fetcher = (url: any) => fetch(url).then((res) => res.json());
 function Page() {
-    const { mutate } = useSWRConfig()
-    const { data, error } = useSWR('http://localhost:8000/items', fetcher)
+  // const { mutate } = useSWRConfig()
+  const { data, error } = useSWR(
+    'http://localhost:8000/items',
+    fetcher
+  );
 
-    const [sreach,setSreach] = React.useState([]);
+  const [search, setSearch] = useState(data);
+  const [kensaku, setKensaku] = useState('');
 
-    useEffect(() => {
-        setSreach(data)
-    },[]);
+  // useEffect(() => {
+  //     setSearch(data)
+  // },[]);
 
-  if (error) return <div>failed to load</div>
-  if (!data) return <div>loading...</div>
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
 
-const handleClick = (e: any) => {
-    const result = data.filter((item:any) => {
-      return item.name.match(e.target.value);
+  const handleClick = (e: any) => {
+    const result = data.filter((item: any) => {
+      return item.name.match(kensaku);
     });
-    console.log(result);
-    setSreach(result);
+    console.log('result', result);
+    setSearch(result);
   };
 
-    return (
-        <>
-        <Head>
+  console.log('search', search);
+
+  return (
+    <>
+      <Head>
         <title>商品一覧</title>
-        </Head>
-        <h1>ラクスヌードル</h1>
-        <p>商品を検索する</p>
+      </Head>
+      <h1>ラクスヌードル</h1>
+      <p>商品を検索する</p>
+      <form>
         <input
-          type='text'
-          onChange={(e) => handleClick(e)}
+          type="text"
+          id="form1"
+          name="kensaku"
+          value={kensaku}
+          onChange={(e) => setKensaku(e.target.value)}
+
+          //   onChange=
+          //   {(e)=>handleClick(e)}
+          //   {(e)=>setSearch(e.target.value)}
         />
-        <button onClick={handleClick}>検索</button>
-        <button>クリア</button>
-        <table>
-    <thead>
-        <tr>
+        <button type="button" onClick={(e) => handleClick(e)}>
+          検索
+        </button>
+        <button  type="button" onClick={()=>setKensaku("")}>クリア</button>
+      </form>
+      <table>
+        <thead>
+          <tr>
             <th>画像</th>
             <th>商品名</th>
             <th>価格</th>
-        </tr>
-    </thead>
-    <tbody>
-        {sreach.map((item:any) => {
-            return (
+          </tr>
+        </thead>
+        <tbody>
+          {!search?.length && (
+            <p>該当の商品がありません</p>
+          )}
+          {!search?.length &&
+            data.map((item: any) => {
+              return (
                 <tr>
-                    <img src={item.img}></img>
-                    <td>{item.name}</td>
-                    <td>{item.price}</td>
+                  <Image src={item.img} width={200} height={143} />
+                  <td>{item.name}</td>
+                  <td>{item.price}</td>
                 </tr>
-            )
-        })}
-    </tbody>
-  </table>
-        </>
-    );
+              );
+            })}
+
+          {search?.length > 0 &&
+            search.map((item: any) => {
+              return (
+                <tr>
+                  <Image src={item.img} width={200} height={143} />
+                  <td>{item.name}</td>
+                  <td>{item.price}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default Page;
