71ba40993c71df21c2c930319c062b01ecda0235
emiri yasuo <emiri.yasuo@rakus-partners.co.jp>
2022-09-01 11:13:29 +0900

    1-5　編集

--- src/pages/items/cartPage.tsx
+++ src/pages/items/cartPage.tsx
@@ -5,7 +5,7 @@ export default function Page() {
     return (
         <>
         <Head>
-          <title>商品一</title>
+          <title>カート</title>
         </Head>
         <CartPage />
         </>
