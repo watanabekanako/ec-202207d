dc6b8e70d923f098775bac15412fa0c1f36c110f
emiri yasuo <emiri.yasuo@rakus-partners.co.jp>
2022-09-01 11:00:17 +0900

    1-5 編集

---
+++ src/pages/items/cartPage.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import CartPage from '../../compornents/cart';
+
+export default function Page() {
+    return (
+        <>
+        <Head>
+          <title>商品一</title>
+        </Head>
+        <CartPage />
+        </>
+    );
+}
